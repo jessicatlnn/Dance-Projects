91002fc8c4522de16eba5821ef2e5522eb71cfb9
Jessica <jessica.tolonen@outlook.com>
2026-07-27 20:33:33 +0300

    Add new item form

--- app.py
+++ app.py
@@ -13,6 +13,26 @@ app.secret_key = config.secret_key
 def index():
     return render_template("index.html")
 
+@app.route("/new_item")
+def new_item():
+    dance_styles = db.query("SELECT * FROM dance_styles")
+    return render_template("new_item.html", dance_styles=dance_styles)
+
+@app.route("/create_item", methods=["POST"])
+def create_item():
+    title = request.form["title"]
+    description = request.form["description"]
+    dance_style_id = request.form["dance_style"]
+    user_id = session["user_id"]
+
+    sql = """INSERT INTO items (title, description, dance_style_id, user_id)
+             VALUES (?, ?, ?, ?)"""
+    db.execute(sql, [title, description, dance_style_id, user_id])
+
+    return redirect("/")
+
+
+
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -44,10 +64,14 @@ def login():
         username = request.form["username"]
         password = request.form["password"]
 
-        sql = "SELECT password_hash FROM users WHERE username = ?"
-        password_hash = db.query(sql, [username])[0][0]
+        sql = "SELECT id, password_hash FROM users WHERE username = ?"
+        result = db.query(sql, [username])[0]
+        user_id = result["id"]
+        password_hash = result["password_hash"]
+
 
         if check_password_hash(password_hash, password):
+            session["user_id"] = user_id
             session["username"] = username
             return redirect("/")
         else:
@@ -55,5 +79,7 @@ def login():
 
 @app.route("/logout")
 def logout():
+    del session["user_id"]
     del session["username"]
     return redirect("/")
+
--- schema.sql
+++ schema.sql
@@ -3,3 +3,25 @@ CREATE TABLE users (
     username TEXT UNIQUE,
     password_hash TEXT
 );
+
+CREATE TABLE dance_styles (
+    id INTEGER PRIMARY KEY,
+    name TEXT
+);
+
+INSERT INTO dance_styles (id, name) VALUES
+(1, 'Hip Hop'),
+(2, 'Commercial'),
+(3, 'Nykytanssi'),
+(4, 'Heels'),
+(5, 'Jazz'),
+(6, 'Baletti'),
+(7, 'Muu');
+
+CREATE TABLE items (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    dance_style_id INTEGER REFERENCES dance_styles(id),
+    user_id INTEGER REFERENCES users
+);
