0ec075de85d0f390f8f584faff02667fef7e7ce2
Jessica <jessica.tolonen@outlook.com>
2026-07-30 15:44:07 +0300

    Add remove items feature

--- items.py
+++ items.py
@@ -29,3 +29,7 @@ def update_item(item_id, title, description, dance_style_id):
                               dance_style_id = ?
                           WHERE id = ?"""
     db.execute(sql, [title, description, dance_style_id, item_id ])
+
+def remove_item(item_id):
+    sql = "DELETE FROM items WHERE id = ?"
+    db.execute(sql, [item_id])
