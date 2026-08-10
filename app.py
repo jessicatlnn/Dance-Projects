728dd7172711e4bd232f245f13cd6f5488cfa726
Jessica <jessica.tolonen@outlook.com>
2026-08-10 23:07:30 +0300

    Check invalid form inputs

--- app.py
+++ app.py
@@ -47,18 +47,31 @@ def new_project():
 @app.route("/create_project", methods=["POST"])
 def create_project():
     require_login()
+
+    dance_styles = db.query("SELECT id, name FROM dance_styles")
+    locations = db.query("SELECT id, name FROM locations ORDER BY name = 'Muu', name")
+
     title = request.form["title"]
-    description = request.form["description"]
-    dance_style_id = request.form["dance_style"]
-    location_ids = request.form.getlist("locations")
-    user_id = session["user_id"]
-
+    if len(title) > 50:
+        abort(403)
     if len(title) < 3:
-        dance_styles = db.query("SELECT id, name FROM dance_styles")
-        locations = db.query("SELECT id, name FROM locations ORDER BY name = 'Muu', name")
-
         return render_template(
             "new_project.html", error="Otsikon täytyy olla vähintään 3 merkkiä pitkä", dance_styles=dance_styles, locations=locations)
+
+    description = request.form["description"]
+    if len(description) > 1000:
+        abort(403)
+    if len(description) < 3:
+        return render_template(
+            "new_project.html", error="Kuvauksen täytyy olla vähintään 3 merkkiä pitkä", dance_styles=dance_styles, locations=locations)
+
+    dance_style_id = request.form["dance_style"]
+
+    location_ids = request.form.getlist("locations")
+    if not location_ids:
+        return render_template("new_project.html", error="Valitse vähintään yksi sijainti", dance_styles=dance_styles, locations=locations)
+
+    user_id = session["user_id"]
 
     projects.add_project(title, description, dance_style_id, location_ids, user_id)
 
@@ -67,9 +80,11 @@ def create_project():
 @app.route("/edit_project/<int:project_id>")
 def edit_project(project_id):
     require_login()
+
     project = projects.get_project(project_id)
     if not project:
         abort(404)
+
     dance_styles = db.query("SELECT id, name FROM dance_styles")
     locations = db.query("SELECT id, name FROM locations ORDER BY name = 'Muu', name")
 
@@ -84,28 +99,42 @@ def edit_project(project_id):
 @app.route("/update_project", methods=["POST"])
 def update_project():
     require_login()
-    project_id = request.form["project_id"]
-    title = request.form["title"]
-    description = request.form["description"]
-    dance_style_id = request.form["dance_style"]
-    location_ids = request.form.getlist("locations")
 
+    project_id = request.form["project_id"]
     project = projects.get_project(project_id)
+    project_locations = projects.get_project_locations(project_id)
+    project_location_ids = [location["id"] for location in project_locations]
     if not project:
         abort(404)
-
     if project["user_id"] != session["user_id"]:
         abort(403)
 
+    dance_styles = db.query("SELECT id, name FROM dance_styles")
+    locations = db.query("SELECT id, name FROM locations ORDER BY name = 'Muu', name")
+
+    title = request.form["title"]
+
+    if len(title) > 50:
+        abort(403)
     if len(title) < 3:
-        dance_styles = db.query("SELECT id, name FROM dance_styles")
-        locations = db.query("SELECT id, name FROM locations ORDER BY name = 'Muu', name")
-
-        project_locations = projects.get_project_locations(project_id)
-        project_location_ids = [location["id"] for location in project_locations]
-
         return render_template("edit_project.html", project=project, dance_styles=dance_styles, locations=locations, project_location_ids=project_location_ids,
                                 error="Otsikon täytyy olla vähintään 3 merkkiä pitkä")
+
+    description = request.form["description"]
+    if len(description) > 1000:
+        abort(403)
+    if len(description) < 3:
+        return render_template("edit_project.html", project=project, dance_styles=dance_styles, locations=locations, project_location_ids=project_location_ids,
+                                error="Kuvauksen täytyy olla vähintään 3 merkkiä pitkä")
+
+    dance_style_id = request.form["dance_style"]
+
+    location_ids = request.form.getlist("locations")
+    if not location_ids:
+        project_locations = projects.get_project_locations(project_id)
+        project_location_ids = [location["id"] for location in project_locations]
+        return render_template("edit_project.html", project=project, dance_styles=dance_styles, locations=locations, project_location_ids=project_location_ids,
+        error="Valitse vähintään yksi sijainti")
 
     projects.update_project(project_id, title, description, dance_style_id, location_ids)
 
@@ -114,10 +143,10 @@ def update_project():
 @app.route("/remove_project/<int:project_id>", methods=["GET", "POST"])
 def remove_project(project_id):
     require_login()
+
     project = projects.get_project(project_id)
     if not project:
         abort(404)
-
     if project["user_id"] != session["user_id"]:
         abort(403)
 
@@ -131,7 +160,6 @@ def remove_project(project_id):
         else:
             return redirect("/project/" + str(project_id))
 
-
 @app.route("/register")
 def register():
     return render_template("register.html")
@@ -139,7 +167,6 @@ def register():
 @app.route("/registration_complete")
 def registration_complete():
     return render_template("registration_complete.html")
-
 
 @app.route("/create", methods=["POST"])
 def create():
@@ -149,13 +176,10 @@ def create():
 
     if len(username) < 3:
         return render_template("register.html", error="Käyttäjänimen täytyy olla vähintään 3 merkkiä pitkä")
-    
     if " " in username:
         return render_template("register.html", error="Käyttäjänimessä ei saa olla välilyöntejä")
-    
     if len(password1) < 6:
         return render_template("register.html", error="Salasanan täytyy olla vähintään 6 merkkiä pitkä")
-    
     if password1 != password2:
         return render_template("register.html", error="Salasanat eivät ole samat")
     
@@ -185,12 +209,10 @@ def login():
         if not result:
             return render_template("login.html", error="Väärä käyttäjätunnus tai salasana")
 
-
         user = result[0]
         user_id = user["id"]
+
         password_hash = user["password_hash"]
-
-
         if check_password_hash(password_hash, password):
             session["user_id"] = user_id
             session["username"] = username
